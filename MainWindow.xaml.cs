c249ceaf5d3cf3bc248d32820273b58393197153
bershakvasya <76277340+bershakvasya@users.noreply.github.com>
2022-04-22 01:19:53 +0300

    Add showing all files in directory

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,15 +27,43 @@ namespace Terminal
             InitializeComponent();
             DevicesManager.AddDisk += Add;
             DevicesManager.RemoveDisk += rem;
+
+            lstB.ContextMenu = new ContextMenu();
+
             DevicesManager.StartLisining();
         }
         private void Add(string text)
         {
-           System.Diagnostics.Debug.WriteLine("add: " + text);
+            Log.Logger.Information("add: {0}", text);
+            System.Diagnostics.Debug.WriteLine("add: " + text);
+
+            Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, () => lstB.Items.Add(text));
+            
         }
         private void rem(string text)
         {
             System.Diagnostics.Debug.WriteLine("remove: " + text);
+            Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, () => lstB.Items.Remove(text));
+        }
+
+        private void lstB_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            string path = (string)(lstB.SelectedItem);
+            string[] allFiles = Directory.GetFiles(path);
+            string temp = "";
+            for (int i = 0; i < allFiles.Length; i++)
+            {
+                //t2 += allFiles[i] + "\n";
+
+                string[] template = allFiles[i].Split('\\');
+                string text = (template[template.Length - 1].Split('.'))[0];
+
+                temp += text+"\n";
+                //bmp = new Bitmap(fullpath);
+
+            }
+           
+            MessageBox.Show(temp); 
         }
     }
 }
